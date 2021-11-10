60afc7d01bc35e2b09b4ef163f1142ffe279b79e
WilkerAlves <wilkeralves97@gmail.com>
2021-11-10 16:35:19 -0300

    criacao de tipo scalar

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -1,8 +1,19 @@
+const { GraphQLScalarType } = require('graphql')
+
 const userResolvers = {
+    DateTime: new GraphQLScalarType({
+        name: 'DateTime',
+        description: "string de data e hora no formato ISO-8601",
+        serialize: (value) => value.toISOString(),
+        parseValue: (value) => new Date(value),
+        parseLiteral: (ast) => new Date(ast.value)
+    }),
+
     Query: {
         users: (root, args, { dataSources }, info) => dataSources.usersAPI.getUsers(),
         user: (root, { id }, { dataSources }, info ) => dataSources.usersAPI.getUserById(id)
     },
+
     Mutation:{
         adicionaUser: async (root, user, { dataSources }, info) => dataSources.usersAPI.adicionaUser(user),
         atualizaUser: async (root, novosDados, { dataSources }, info) => dataSources.usersAPI.atualizaUser(novosDados),
