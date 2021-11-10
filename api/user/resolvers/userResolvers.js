b6354a73909bb41d2d564d6c0cdfc31dcf826f3c
WilkerAlves <wilkeralves97@gmail.com>
2021-11-10 16:19:59 -0300

    mutation

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -2,6 +2,11 @@ const userResolvers = {
     Query: {
         users: (root, args, { dataSources }, info) => dataSources.usersAPI.getUsers(),
         user: (root, { id }, { dataSources }, info ) => dataSources.usersAPI.getUserById(id)
+    },
+    Mutation:{
+        adicionaUser: async (root, user, { dataSources }, info) => dataSources.usersAPI.adicionaUser(user),
+        atualizaUser: async (root, novosDados, { dataSources }, info) => dataSources.usersAPI.atualizaUser(novosDados),
+        deletaUser: async (root, { id }, { dataSources }, info) => dataSources.usersAPI.deletaUser(id)
     }
 
 }
